1d5b11b180494c082beef5429e6afccbd9077766
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-07-26 09:02:21 -0700

    completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,23 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+let consume = (arg1,arg2,cb)=>{return cb(arg1,arg2);}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add = (arg1, arg2)=>{return arg1+arg2;}
 
+let multiply = (arg1, arg2)=>{return arg1*arg2;}
+
+let greeting = (first, last)=>{return `Hello ${first} ${last}, nice to meet you!` }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +30,8 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+//nested Function can acess internal becasue it is declared in its parent scope.
+//Functions have acess to variables and methods decalred in all scopes parent to it all up the way to window/global scope.
 
 
 const external = "I'm outside the function";
